e367b703c59709c02258de70f40cbd7fcee93f5c
Pelayo <pela260@gmail.com>
2023-02-16 12:12:46 +0100

    cuarto commit

---
+++ Onion.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Onion here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Onion
+{
+    // instance variables - replace the example below with your own
+    private int onionNum;
+    private boolean isCut;
+    private Layer layer;
+    
+}
