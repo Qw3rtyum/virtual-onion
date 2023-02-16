c5861b555fb618b4a9cbcf8b9acb95928b6530c3
Qw3rtyum <pablolylm@gmail.com>
2023-02-16 10:54:17 +0100

    Commit inicial

---
+++ Layer.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class Layer here.
+ * 
+ * @author Qw3rtyum 
+ * @version 02.16.23
+ */
+public class Layer
+{
+    private int layerNumber;
+    private double thickness;
+
+    /**
+     * Constructor de la clase Layer
+     */
+    public Layer(int newLayerNum, double newThickness)
+    {
+        layerNumber = newLayerNum;
+        thickness = newThickness;
+    }
+}
